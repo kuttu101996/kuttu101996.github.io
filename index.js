f63f89f0f7919d545a44ba860667913ae9362fa1
kuttu101996 <kuttulr1996@gmail.com>
2023-09-24 00:38:31 +0530

    communi-cate api integrated in contact section

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-    
 // window.onscroll = function() {scrollFunction()};
 
 // function scrollFunction() {
@@ -10,7 +9,7 @@
 //     // for (let x in a){
 //     //   a[x].style.color = 'black'
 //     // }
-    
+
 //     // document.getElementById("logo").style.fontSize = "25px";
 //   }
 //   else if ((document.body.scrollTop > 1680 && document.body.scrollTop < 2580) || (document.documentElement.scrollTop > 1680 && document.documentElement.scrollTop < 2580)){
@@ -38,63 +37,94 @@
 //   }
 // }
 
-let nav_menu = document.getElementById("nav-menu")
+let nav_menu = document.getElementById("nav-menu");
 let togglePage = document.getElementById("toggle_navPageFull");
 let togglePageHalf = document.getElementById("toggle_navPageHalf");
 let clBtn = document.getElementById("close");
 let burger = document.getElementById("burger");
-burger.addEventListener("click", ()=>{
-    open();
-})
-function open(){
-    togglePage.style.left = '0%';
-    togglePage.style.height = '100%'
-    togglePage.style.transition = '.6s ease-in-out';
-    burger.style.display = 'none';
-    // nav_menu.style.display = 'none';
+burger.addEventListener("click", () => {
+  open();
+});
+function open() {
+  togglePage.style.left = "0%";
+  togglePage.style.height = "100%";
+  togglePage.style.transition = ".6s ease-in-out";
+  burger.style.display = "none";
+  // nav_menu.style.display = 'none';
 }
-function clo(){  
-    togglePage.style.left = '-100%';
-    burger.style.display = 'block';
-    // nav_menu.style.display = 'block';
+function clo() {
+  togglePage.style.left = "-100%";
+  burger.style.display = "block";
+  // nav_menu.style.display = 'block';
 }
-clBtn.addEventListener("click", ()=>{
-    clo();
-})
+clBtn.addEventListener("click", () => {
+  clo();
+});
 
-function dis (){
-    document.getElementById("res_btn").disabled = true
+function dis() {
+  document.getElementById("res_btn").disabled = true;
 }
-
 
 // Responsive Social Links
-let responsive_social = document.getElementById("responsive_social")
+let responsive_social = document.getElementById("responsive_social");
 
 let arrow_right_id = document.getElementById("arrow_right_id");
 arrow_right_id.style.display = "none";
 
-let link_icons_container = document.getElementById("link_icons_container")
-link_icons_container.style.display = 'none';
+let link_icons_container = document.getElementById("link_icons_container");
+link_icons_container.style.display = "none";
 
 let arrow_left_id = document.getElementById("arrow_left_id");
-arrow_left_id.addEventListener("click", ()=>{
-    responsive_social.style.left = "-7%";
-    arrow_left_id.style.transition = '.6s ease-in-out';
-    arrow_right_id.style.display = "block";
-    arrow_left_id.style.display = "none";
-    link_icons_container.style.display = 'block';
-    style(arrow_right_id)
-})
-arrow_right_id.addEventListener("click", ()=>{
-    responsive_social.style.left = "5%";
-    arrow_left_id.style.display = "block";
-    arrow_right_id.style.display = "none";
-    style(arrow_left_id);
-    link_icons_container.style.display = "none";
-})
+arrow_left_id.addEventListener("click", () => {
+  responsive_social.style.left = "-7%";
+  arrow_left_id.style.transition = ".6s ease-in-out";
+  arrow_right_id.style.display = "block";
+  arrow_left_id.style.display = "none";
+  link_icons_container.style.display = "block";
+  style(arrow_right_id);
+});
+arrow_right_id.addEventListener("click", () => {
+  responsive_social.style.left = "5%";
+  arrow_left_id.style.display = "block";
+  arrow_right_id.style.display = "none";
+  style(arrow_left_id);
+  link_icons_container.style.display = "none";
+});
 
-function style(param){
-    param.style.display = "flex";
-    param.style.alignItems = "center";
-    param.style.justifyContent = "center";
+function style(param) {
+  param.style.display = "flex";
+  param.style.alignItems = "center";
+  param.style.justifyContent = "center";
 }
+
+const msgForm = document.getElementById("msgForm");
+msgForm.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  let obj = {
+    name: document.getElementById("userName").value,
+    email: document.getElementById("userEmail").value,
+    mobile: document.getElementById("userMobile").value,
+    message: document.getElementById("msgContent").value,
+  };
+
+  await fetch(
+    `https://cc-yqsx.onrender.com/api/message/650d6606f2914ea3852ee217`,
+    {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(obj),
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+        if (data.message === 'Successfully send' && data.password) {
+            alert(`Try https://communi-cate-update.netlify.app a browser based chatting application, ${data.password}`)
+        }
+        else if (data.message === 'Successfully send') {
+            alert(
+              `Message successfully send, try https://communi-cate-update.netlify.app, if you are a new user try your email as your password you can change it anytime.`
+            );
+        }
+    });
+});
